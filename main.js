7df59319a2286fa345cf0efabcee85cca51d18c1
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-10-31 23:03:17 +0100

    ADJ: Time text and hour hand adjusted

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const secondHand = document.querySelector(".sec-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+const timeDisplay = document.querySelector(".time");
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -12,9 +14,12 @@ function setDate() {
 
   const secondsDegree = (seconds / 60) * 360 + 90;
   const minuteDegree = (minutes / 60) * 360 + 90;
-  const hourDegree = (hour / 24) * 360 + 90;
+  const hourDegree = (hour / 12) * 360 + 90;
 
   console.log(seconds, minutes, hour);
+
+  timeDisplay.textContent = `The time is: ${hour}: ${minutes}`;
+
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
   hourHand.style.transform = `rotate(${hourDegree}deg)`;
